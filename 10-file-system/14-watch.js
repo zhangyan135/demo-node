f0c961649db108c57d24d1d4c9f08e5071a659d8
zhangyan135 <38712674+zhangyan135@users.noreply.github.com>
2019-09-20 15:24:33 +0800

    Update 14-watch.js

--- 10-file-system/14-watch.js
+++ 10-file-system/14-watch.js
@@ -3,5 +3,5 @@
 const fs=require('fs'),
       dst=process.argv[2];
 fs.watch(dst,function(evt,file){
-  console.log("%evt" happened with file:'%s",evt,file);
+  console.log("%evt happened with file: %s",evt,file);
 });
